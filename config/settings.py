2d39baa8a9ce5debd02b19dc2388e7d0b081b929
shunNel <noelgld13@gmail.com>
2022-01-04 13:53:15 +0900

    .500エラー調査_6

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import dj_database_url
 import os
 import environ
+import django_heroku
 
 
 
@@ -16,15 +17,13 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 INSTALLED_APPS = [
 
-    # ***** Local apps *****
     'todo.apps.TodoConfig',
     'accounts.apps.AccountsConfig',
 
-    # ***** Third Party apps *****
     'widget_tweaks',    #DOM 属性等の操作
     # 'drf_writable_nested',
 
-    # ***** Django apps *****
+
     # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,13 +39,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -54,7 +54,6 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [],
         'DIRS': [   
                     os.path.join(BASE_DIR, "accounts", "templates"),
                     os.path.join(BASE_DIR, "todo", "templates"),
@@ -82,8 +81,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -111,10 +109,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),    
 )
@@ -138,17 +132,21 @@ except ImportError:
     pass
 
 
-# DATABASES['default'] = dj_database_url.config()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 
 if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    django_heroku.settings(locals())
+    
+
+    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
+
     env = environ.Env()
     env.read_env(os.path.join(BASE_DIR, '.env'))
     SECRET_KEY = env('SECRET_KEY')
 
-    import django_heroku
-    django_heroku.settings(locals())
+    
+    
 
 
-DEBUG = True
+DEBUG = False
